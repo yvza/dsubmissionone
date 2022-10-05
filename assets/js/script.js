cc949e7a5084f7a744db7675be3d59f808e2d113
yuza <akhmadyuza@gmail.com>
2022-10-05 14:37:36 +0700

    adding interactivity to overlay menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 'use strict';
 
 document.getElementById('year').innerHTML = new Date().getFullYear()
+
+let hamburgerMenu = document.getElementsByClassName('list-wrapper')[0]
+hamburgerMenu.addEventListener('click', function() {
+    window.scrollTo(0, 0)
+    let grabMenu = document.getElementsByClassName('overlay-menu')[0]
+    let grabBody = document.getElementsByTagName('body')[0]
+    grabMenu.style.visibility = 'visible'
+    grabBody.style.overflowY = 'hidden'
+})
+
+let closeMenu = document.getElementsByClassName('bi bi-x')[0]
+closeMenu.addEventListener('click', function() {
+    window.scrollTo(0, 0)
+    let grabMenu = document.getElementsByClassName('overlay-menu')[0]
+    let grabBody = document.getElementsByTagName('body')[0]
+    grabMenu.style.visibility = 'hidden'
+    grabBody.style.overflowY = 'scroll'
+})
